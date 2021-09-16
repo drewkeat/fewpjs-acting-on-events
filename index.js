2dad5eb71fe63850b1d4511dd9a1b9b9f3f59bd2
Drew Keat <thedrewkeat@gmail.com>
2021-09-15 21:12:23 -0600

    commit tree 5a767cbcdc5f9fd1f11275adf4d8df1f3bf1626c

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 // Your code here
+
+function moveDodgerLeft() {
+    const left = parseInt(dodger.style.left)
+    if (left > 0) {
+    dodger.style.left = `${left - 5}px`;
+    };
+};
+
+function moveDodgerRight() {
+    const left = parseInt(dodger.style.left)
+    if (left < 360){
+    dodger.style.left = `${left + 5}px`;
+    };
+}
+
+function moveDodgerUp() {
+    const left = parseInt(dodger.style.left)
+    if (left < 360){
+    dodger.style.left = `${left + 5}px`;
+    };
+}
+
+
+document.addEventListener("keydown", function(e) {
+    if (e.key === "ArrowLeft") {
+        moveDodgerLeft();
+    } else if (e.key === "ArrowRight") {
+        moveDodgerRight();
+    };
+    console.log(e.key)
+});
