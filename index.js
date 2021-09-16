7f4d12bfb0ad296e9cbc981075503a43c4142c4f
Drew Keat <thedrewkeat@gmail.com>
2021-09-15 20:34:21 -0600

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Your code here
+
+function moveDodgerLeft() {
+    let left = parseInt(dodger.style.left)
+    if (dodger.style.left != "0px") {
+        dodger.style.left = `${left - 1}px`
+    };
+};
+
+function moveDodgerRight() {
+    let left = parseInt(dodger.style.left)
+    if (dodger.style.left != "360px") {
+        dodger.style.left = `${left + 1}px`
+    };
+};
+
+addEventListener("keydown", function(e){
+    if (e.key === "ArrowLeft") {
+        moveDodgerLeft();
+    } else if (e.key === "ArrowRight") {
+       moveDodgerRight(); 
+    };
+    console.log(e.key)
+});
